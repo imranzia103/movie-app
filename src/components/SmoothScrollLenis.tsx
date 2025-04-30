4dafe4a24d664c55ba17246697af29a927d4bba6
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-04-30 13:56:43 +0500

    MOTION COMPLETE

--- src/components/SmoothScrollLenis.tsx
+++ src/components/SmoothScrollLenis.tsx
@@ -1,6 +1,8 @@
-
+import { useTransform, useMotionTemplate } from "framer-motion";
 import { FiArrowDownRight } from "react-icons/fi";
 import { SiSpacex } from "react-icons/si";
+import { motion, useScroll } from "framer-motion";
+import { div } from "framer-motion/client";
 
 export const SmoothScrollLenis = () => {
   return (
@@ -41,21 +43,110 @@ const Hero = () => {
       style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}
     >
       <CenterImage />
+      <ParallaxImages />
+
+
+      
+    <div className="absolute bottom-0 left-0 right-0 
+    h-96 bg-gradient-to-b from-zinc-950/0
+     to-bg-zinc-950"/>
+    
+    
     </div>
   );
 };
 
 const CenterImage = () => {
+  const { scrollY } = useScroll();
+
+  const clip1 = useTransform(scrollY, [0, SECTION_HEIGHT], [25, 0]);
+
+  const clip2 = useTransform(scrollY, [0, SECTION_HEIGHT], [75, 100]);
+
+  const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`;
+
+  const opacity = useTransform(
+    scrollY,
+    [SECTION_HEIGHT, SECTION_HEIGHT + 500],
+    [1, 0]
+  );
+
+  const backgroundSize = useTransform(
+    scrollY,
+    [0, SECTION_HEIGHT + 500],
+    ["170%", "100%"]
+  );
+
   return (
-    <div
+    <motion.div
       className="sticky top-0 h-screen w-full"
       style={{
-        backgroundImage:
-        "url('../src/assets/space.jpg')",
+        opacity,
+        backgroundSize,
+        clipPath,
+
+        backgroundImage: "url('../src/assets/space.jpg')",
 
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     />
   );
+};
+
+
+
+const ParallaxImages =() =>{
+    return ( 
+
+<div className=" relative z-10 mx-auto  max-w-5xl px-4
+ pt[200px]">
+    
+    
+    
+    <ParallaxImg
+    src="../src/assets/rocket.jpg"
+    alt="an example of Space Launch"
+    start={-200}
+    end={200}
+    className="w-1/3"/>
+
+
+<ParallaxImg
+    src="../src/assets/astronaunt.jpg"
+    alt="an example of Space Launch"
+    start={-200}
+    end={200}
+    className="w-1/3"/>
+    
+    
+    
+    
+    
+    </div>
+
+
+
+    )
+
+}
+
+
+const ParallaxImg = ({
+    className, 
+    alt, 
+    src,
+    start, 
+    end,
+}: {
+    className: string;
+    alt:string;
+
+    src:string;
+    start: number;
+    end:number;
+
+}) => {
+    return <motion.img src={src} alt={alt} className=
+{className} />
 };
