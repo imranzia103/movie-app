77f810bc313a9b850637a4c61b20367bd817e9e2
Imran Zia <imranzia103@gmail.com>
2025-05-30 19:28:06 +0500

    Add files via upload
    authication of users

--- userRoutes.js
+++ userRoutes.js
@@ -3,15 +3,40 @@ import express from "express";
 
 // controllers
 
-import { createUser, loginUser } from "../controllers/userController.js";
 
-//misslewares
+import { 
+    createUser,
+    loginUser,
+    logoutCurrentUser,
+    getAllUsers,
+    getCurrentUserProfile.
+    updateCurrentUserProfile 
+    
+    
+
+} from "../controllers/userController.js";
+
+//middlewares
+
+import {authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js'
+
+
 
 
 const router = express.Router();
 
 
-router.route("/").post(createUser);
+router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers)
+
+router.post ('/auth', loginUser)
+
+router.post ('/logout', logoutCurrentUser)
+
+
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate,
+    updateCurrentUserProfile)
+
+)
 
 
 
