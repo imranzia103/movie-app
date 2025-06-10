73c58cb8726b91574eef7b71081f52962cddae2d
Imran Zia <imranzia103@gmail.com>
2025-06-10 21:45:21 +0500

    Add files via upload
    Genre added

---
+++ Genre.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        maxLength: 32,
+    }
+})
+
+export default mongoose.model("Genre", genreSchema);
