bae41acf02345e12ea772027096c6640594b8580
Imran Zia <imranzia103@gmail.com>
2025-07-03 13:25:06 +0500

    Add files via upload
    token frontend

--- App.jsx
+++ App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Outlet } from "react-router-dom";
+import Navigation from "./components/pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
--- main.jsx
+++ main.jsx
@@ -2,9 +2,59 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import store from "./components/redux/store.js";
+import { Provider } from 'react-redux';
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+
+//Auth
+
+
+
+//Resticted
+
+import Login from './components/pages/Auth/Login.jsx';
+import Register from './components/pages/Auth/Register.jsx';
+import PrivateRoute from './components/pages/Auth/PrivateRoute.jsx';
+import Profile from "./components/pages/User/Profile.jsx"
+
+
+
+import Home from './components/pages/Home.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements( 
+    <Route path="/" element={<App />}>
+          <Route index={true} path="/" element={<Home />} />
+          <Route path='/login' element={<Login/>} />
+           <Route path='/register' element={<Register/>} />
+
+
+
+           <Route path='' element={<PrivateRoute/>}>
+           <Route path='/profile' element={<Profile/>}/>
+
+
+
+
+
+
+           </Route>
+
+
+
+  </Route>)
+
+);
 
 createRoot(document.getElementById('root')).render(
   
-    <App />
+  <Provider store={store}>
+    
+      <RouterProvider router={router} />
+    
+  </Provider>
   
 )
